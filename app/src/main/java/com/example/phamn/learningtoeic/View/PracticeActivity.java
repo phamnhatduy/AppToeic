f6cf347d9879f6ce3b71123d2d50f2135bae75d0
thainguyen <dinhthainguyen97@gmail.com>
2019-05-13 22:37:43 +0700

    10:32

--- app/src/main/java/com/example/phamn/learningtoeic/View/PracticeActivity.java
+++ app/src/main/java/com/example/phamn/learningtoeic/View/PracticeActivity.java
@@ -11,6 +11,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,10 +21,12 @@ import java.util.Random;
 
 
 public class PracticeActivity extends AppCompatActivity {
-    ImageView imgStart;
+    SoundManager soundManager;
+    ImageView imgStart,imgAns;
     EditText edtAns;
-    TextView txtQues,txtTrans;
+    TextView txtQues,txtTrans,txtLevel;
     Button btnConfirm,btnNext;
+    ProgressBar progressQues;
     Random random;
     int i;
     int countClick;
@@ -35,97 +38,236 @@ public class PracticeActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Learning Toeic");
 
         imgStart=findViewById(R.id.img_play);
+        imgAns=findViewById(R.id.img_ans);
         edtAns=findViewById(R.id.edt_ans);
         txtQues=findViewById(R.id.edt_question);
         txtTrans=findViewById(R.id.txt_translate);
         btnConfirm=findViewById(R.id.btn_confirm);
         btnNext=findViewById(R.id.btn_next);
+        progressQues=findViewById(R.id.progress_ques);
+        txtLevel=findViewById(R.id.txt_level);
         final Animation animation = AnimationUtils.loadAnimation(this,R.anim.scale_sound);
-        final String[] quesData = {"Traffic is ____ freely on the motorway.","This photo shows us a busy rush hour scene"};
-        final String[] ansData = {"flowing","shows"};
+        final String[] quesData = {"Traffic is ____ freely on the motorway.","This photo ___ us a busy rush hour scene.",
+                "Several people can be seen ___ to cross the road.","It looks as if it is ___ to rain.",
+                "The ___ are all brightly coloured."};
+        final  String[] quesData2 ={"The ___ on the left is riding a pedal cycle.",
+                "All the people in the ___ are waving their arms"," The water is ___ green in colour.",
+                "You can only see four ___ clearly.","You can see one man getting off the ___."};
+        final String[] ansData = {"flowing","shows","trying","going","buildings"};
+        final String[] ansData2 ={"cyclist","motorboats","deep","boats","tram"};
         final String[] fullansData={"Traffic is flowing freely on the motorway.","This photo shows us a busy rush hour scene."};
         final String[] transData={"Giao thông đang lưu thông tự do trên đường cao tốc.",
-                "Bức ảnh này cho chúng ta thấy một khung cảnh giờ cao điểm bận rộn."};
+                "Bức ảnh này cho chúng ta thấy một khung cảnh giờ cao điểm bận rộn.",
+                "Một số người có thể được nhìn thấy đang cố gắng băng qua đường",
+                "Có vẻ như trời sắp mưa.","Các tòa nhà đều có màu sắc rực rỡ."};
+        final String[] transData2 = {"Người đi xe đạp bên trái đang đạp xe đạp.","Tất cả những người trong xuồng máy đang vẫy tay.",
+                "Nước có màu xanh đậm.","Bạn chỉ có thể nhìn thấy bốn chiếc thuyền rõ ràng.","Bạn có thể thấy một người đàn ông xuống xe điện."};
+        soundplay();
         random=new Random();
-        i=random.nextInt(2);
+        i=random.nextInt(5);
 
-     /*   final int songId = this.getIdSong(soundData[i]);
+     /* final int songId = this.getIdSong(soundData[i]);
         final MediaPlayer mediaPlayer = MediaPlayer.create(this,songId);*/
         final MediaPlayer mp1 = MediaPlayer.create(this,R.raw.ques1);
-        final MediaPlayer mp2=MediaPlayer.create(this,R.raw.ques2);
-
-        txtQues.setText(quesData[i]);
+        final MediaPlayer mp2 = MediaPlayer.create(this,R.raw.ques2);
+        final MediaPlayer mp3 = MediaPlayer.create(this,R.raw.ques3);
+        final MediaPlayer mp4 = MediaPlayer.create(this,R.raw.ques4);
+        final MediaPlayer mp5 = MediaPlayer.create(this,R.raw.ques5);
+        final MediaPlayer mp6 = MediaPlayer.create(this,R.raw.ques6);
+        final MediaPlayer mp7= MediaPlayer.create(this,R.raw.ques7);
+        final MediaPlayer mp8 = MediaPlayer.create(this,R.raw.ques8);
+        final MediaPlayer mp9 = MediaPlayer.create(this,R.raw.ques9);
+        final MediaPlayer mp10 = MediaPlayer.create(this,R.raw.ques10);
+        final MediaPlayer mpAns1 = MediaPlayer.create(this,R.raw.ans1);
+        final MediaPlayer mpAns2 = MediaPlayer.create(this,R.raw.ans2);
+        final MediaPlayer mpAns3 = MediaPlayer.create(this,R.raw.ans3);
+        final MediaPlayer mpAns4 = MediaPlayer.create(this,R.raw.ans4);
+        final MediaPlayer mpAns5 = MediaPlayer.create(this,R.raw.ans5);
+        final MediaPlayer mpAns6 = MediaPlayer.create(this,R.raw.ans6);
+        final MediaPlayer mpAns7 = MediaPlayer.create(this,R.raw.ans7);
+        final MediaPlayer mpAns8 = MediaPlayer.create(this,R.raw.ans8);
+        final MediaPlayer mpAns9 = MediaPlayer.create(this,R.raw.ans9);
+        final MediaPlayer mpAns10 = MediaPlayer.create(this,R.raw.ans10);
+        if(txtLevel.getText().equals("Level 1")) {
+            txtQues.setText(quesData[i]);
+        }if(txtLevel.getText().equals("Level 2")){
+            txtQues.setText(quesData2[i]);
+        }
         imgStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(i==0)
-                {
-                    mp1.start();
+                if(txtLevel.getText().equals("Level 1")) {
+                    if (i == 0) {
+                        mp1.start();
+                    }
+                    if (i == 1) {
+                        mp2.start();
+                    }
+                    if (i == 2) {
+                        mp3.start();
+                    }
+                    if (i == 3) {
+                        mp4.start();
+                    }
+                    if (i == 4) {
+                        mp5.start();
+                    }
                 }
-                if(i==1)
+                if(txtLevel.getText().equals("Level 2"))
                 {
-                    mp2.start();
+                    if(i==0){mp6.start();}
+                    if(i==1){mp7.start();}
+                    if(i==2){mp8.start();}
+                    if(i==3){mp9.start();}
+                    if(i==4){mp10.start();}
                 }
                 v.startAnimation(animation);
-                imgStart.setImageResource(R.drawable.sound_icon);
+                imgStart.setImageResource(R.drawable.icon_sound);
             }
         });
         btnConfirm.setOnClickListener(new View.OnClickListener() {
+            //String ans = edtAns.getText().toString().trim();
             @Override
             public void onClick(View v) {
-                countClick+=1;
                 String ans = edtAns.getText().toString().trim();
+                countClick+=1;
+                if (ans.equals("")) {
+                    Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                }
                 if(countClick==1)
                 {
-                    if(ans.equals(""))
-                    {
-                        Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                    if(txtLevel.getText().equals("Level 1")) {
+                        if (ans.equalsIgnoreCase(ansData[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
-                    else if(ans.equalsIgnoreCase(ansData[i]))
+                    if(txtLevel.getText().equals("Level 2"))
                     {
-                        Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
-                        edtAns.getText().clear();
+                        if (ans.equalsIgnoreCase(ansData2[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
                 }
                 if(countClick==2)
                 {
-                    txtTrans.setText(transData[i]);
-                    txtTrans.setVisibility(View.VISIBLE);
-                    if(ans.equals(""))
+                    imgAns.setVisibility(View.VISIBLE);
+                    imgAns.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            v.startAnimation(animation);
+                            if(txtLevel.getText().equals("Level 1"))
+                            {
+                                if(i==0){mpAns1.start();}
+                                if(i==1){mpAns2.start();}
+                                if(i==2){mpAns3.start();}
+                                if(i==3){mpAns4.start();}
+                                if(i==4){mpAns5.start();}
+
+                            }
+                            if(txtLevel.getText().equals("Level 2"))
+                            {
+                                if(i==0){mpAns6.start();}
+                                if(i==1){mpAns7.start();}
+                                if(i==2){mpAns8.start();}
+                                if(i==3){mpAns9.start();}
+                                if(i==4){mpAns10.start();}
+                            }
+                        }
+                    });
+                    if(txtLevel.getText().equals("Level 1"))
                     {
-                        Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        if(ans.equals(""))
+                        {
+                            //Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(ans.equalsIgnoreCase(ansData[i]))
+                        {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
-                    else if(ans.equalsIgnoreCase(ansData[i]))
+                    if(txtLevel.getText().equals("Level 2"))
                     {
-                        Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
-                    }
-                    else
-                    {
-                        Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
-                        edtAns.getText().clear();
+                        if(ans.equals(""))
+                        {
+                            //Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(ans.equalsIgnoreCase(ansData2[i]))
+                        {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
                 }
-                if(countClick>=3)
+                if(countClick ==3)
                 {
-                    txtTrans.setText(ansData[i]);
-                    txtTrans.setVisibility(View.VISIBLE);
-                    if(ans.equals(""))
-                    {
-                        Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+
+                    if(txtLevel.getText().equals("Level 1")) {
+                        txtTrans.setText(transData[i]);
+                        txtTrans.setVisibility(View.VISIBLE);
+                        if (ans.equals("")) {
+                            // Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        } else if (ans.equalsIgnoreCase(ansData[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
-                    else if(ans.equalsIgnoreCase(ansData[i]))
+                    if(txtLevel.getText().equals("Level 2"))
                     {
-                        Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        txtTrans.setText(transData2[i]);
+                        txtTrans.setVisibility(View.VISIBLE);
+                        if (ans.equals("")) {
+                            // Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        } else if (ans.equalsIgnoreCase(ansData2[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
-                    else
+                }
+                if(countClick>=4)
+                {
+                    if(txtLevel.getText().equals("Level 1")) {
+                        txtTrans.setText(ansData[i]);
+                        txtTrans.setVisibility(View.VISIBLE);
+                        if (ans.equals("")) {
+                            // Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        } else if (ans.equalsIgnoreCase(ansData[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
+                    }
+                    if(txtLevel.getText().equals("Level 2"))
                     {
-                        Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
-                        edtAns.getText().clear();
+                        txtTrans.setText(ansData2[i]);
+                        txtTrans.setVisibility(View.VISIBLE);
+                        if (ans.equals("")) {
+                            //Toast.makeText(PracticeActivity.this, "Please insert", Toast.LENGTH_SHORT).show();
+                        } else if (ans.equalsIgnoreCase(ansData2[i])) {
+                            Toast.makeText(PracticeActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(PracticeActivity.this, "false", Toast.LENGTH_SHORT).show();
+                            edtAns.getText().clear();
+                        }
                     }
                 }
 
@@ -133,41 +275,72 @@ public class PracticeActivity extends AppCompatActivity {
 
         });
         btnNext.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
+                int current = progressQues.getProgress();
                 countClick=0;
-                i =random.nextInt(2);
-                txtQues.setText(quesData[i]);
-                if(i==0)
+                i =random.nextInt(5);
+                if(txtLevel.getText().equals("Level 1")&& current<progressQues.getMax())
                 {
-                    mp1.start();
+                    txtQues.setText(quesData[i]);
+                    if(i==0){mp1.start();}
+                    if(i==1){mp2.start();}
+                    if(i==2){mp3.start();}
+                    if(i==3){mp4.start();}
+                    if(i==4){mp5.start();}
+
                 }
-                if(i==1)
+                else if(current>=progressQues.getMax())
                 {
-                    mp2.start();
+                    current=0;
+                    txtLevel.setText("Level 2");
+                    txtQues.setText(quesData2[i]);
+                    if(i==0){mp6.start();}
+                    if(i==1){mp7.start();}
+                    if(i==2){mp8.start();}
+                    if(i==3){mp9.start();}
+                    if(i==4){mp10.start();}
                 }
+                else
+                {
+                    txtQues.setText(quesData2[i]);
+                    if(i==0){mp6.start();}
+                    if(i==1){mp7.start();}
+                    if(i==2){mp8.start();}
+                    if(i==3){mp9.start();}
+                    if(i==4){mp10.start();}
+                }
+                progressQues.setProgress(current+30);
                 txtTrans.setVisibility(View.INVISIBLE);
                 edtAns.getText().clear();
+                imgAns.setVisibility(View.INVISIBLE);
             }
         });
 
     }
-   /* public int getIdSong(String resName)
+    /* public int getIdSong(String resName)
+     {
+         String packName = this.getPackageName();
+         int resId = this.getResources().getIdentifier(resName,"raw",packName);
+         return resId;
+     }*/
+    public void soundplay()
     {
-        String packName = this.getPackageName();
-        int resId = this.getResources().getIdentifier(resName,"raw",packName);
-        return resId;
-    }*/
-   @Override
-   public boolean onOptionsItemSelected(MenuItem item) {
-       switch (item.getItemId()) {
-           case android.R.id.home:
-               this.finish();
-               return true;
-           default:
-               return super.onOptionsItemSelected(item);
-       }
-   }
+        soundManager = new SoundManager();
+        soundManager.initSounds(getBaseContext());
+        soundManager.addSound(0,R.raw.ans1);
+        soundManager.addSound(1,R.raw.ans2);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
